96cd610802d8977cb2e63cbd82e8e579afb9fc7c
offiicialkamal <indianhealper@gmail.com>
2025-11-27 16:17:13 +0000

    first offiicial plugin version

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 "use strict";
 (() => {
+  const editorManager = acode.require("editorMnager");
+  const sideBarApps = acode.require("sidebarApps");
+  const SideButton = acode.require("sideButton");
 
+  var e = {
+    id: "offiicialkamal.pdfViewer",
+  };
 
-    class pdfViewer {
+  class managerFunctions {
+    #currentFile() {
+      return editorManager.activeFile;
+    }
+  }
 
-        async init() {}
-        async destroy() {}
+  class eventsHandler {}
+
+  class pdfViewer {
+    constructor() {}
+    async init() {
+      editorManager.on("switch-file", () => {});
     }
 
+    async destroy() {}
+  }
 
-    if (window.acode) {
-        let i = new pdfViewer();
-        acode.setPluginInit(e.id, async (n, o, {
-            cacheFileUrl: s, cacheFile: d
-        }) => {
-            n.endsWith("/") || (n += "/");
-            i.baseUrl = n;
-            await i.init(o, d, s);
-        });
-        acode.setPluginUnmount(e.id, () => {
-            i.destroy();
-        });
-    }
+  if (window.acode) {
+    let i = new pdfViewer();
+    acode.setPluginInit(
+      e.id,
+      async (n, o, { cacheFileUrl: s, cacheFile: d }) => {
+        n.endsWith("/") || (n += "/");
+        i.baseUrl = n;
+        await i.init(o, d, s);
+      }
+    );
+    acode.setPluginUnmount(e.id, () => {
+      i.destroy();
+    });
+  }
 })();
